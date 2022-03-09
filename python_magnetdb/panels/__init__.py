8e53fbd2d3c11950876f78cccef170cfa36ce0d8
Christophe Trophime <christophe.trophime@lncmi.cnrs.fr>
2022-03-09 16:28:01 +0000

    prepare for more features in Analytical section

--- python_magnetdb/panels/__init__.py
+++ python_magnetdb/panels/__init__.py
@@ -1,11 +1,13 @@
 from .panel_mrecord import MRecordPanel
 from .panel_bmap import BMapPanel
+from .panel_stress import StressPanel
 
 # Fill out with new models
 _models = [
     #  [url, title, model]
     ["mrecordpanel", "MRecord Panel", MRecordPanel],
-    ["bmappanel", "Bmap Panel", BMapPanel]
+    ["bmappanel", "Bmap Panel", BMapPanel],
+    ["stressmappanel", "Stress Panel", StressPanel]
 ]
 
 titles = {m[0]: m[1] for m in _models}
